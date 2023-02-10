870552f03a55348f834ae63eb9d8281608314dcf
H Z Wei <hzwei92@gmail.com>
2023-02-10 00:33:55 -0800

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
 import { AuthModule } from './auth/auth.module';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -67,7 +65,5 @@ import { PinsModule } from './pins/pins.module';
     LinksModule,
     PinsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
